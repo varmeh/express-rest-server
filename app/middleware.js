1391747df58d1987d675db10fe6a007dbf731bfe
varmeh <varun.sagi20@gmail.com>
2019-07-17 17:45:53 +0530

    CORS handling

--- app/middleware.js
+++ app/middleware.js
@@ -3,13 +3,6 @@ const path = require('path')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 
-module.exports = app => {
-	// Configure logging first
-	logger(app)
-
-	app.use(bodyParser.json())
-}
-
 /* Logging Configuration */
 function logger(app) {
 	// Log on Terminal
@@ -23,4 +16,25 @@ function logger(app) {
 		}
 	)
 	app.use(morgan('combined', { stream: accessLogStream }))
+}
+
+/* CORS Handling */
+const corsHandling = (req, res, next) => {
+	// Specify Clients from which Cross Origin Requests are accepted
+	res.setHeader('Access-Control-Allow-Origin', 'localhost:4000, localhost:8080')
+
+	// Specify Methods which are accessible from cross-origin requests
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE')
+
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+
+	next()
+}
+
+module.exports = app => {
+	// Configure logging first
+	logger(app)
+
+	app.use(bodyParser.json())
+	app.use(corsHandling)
 }
