5dad9b53a0c0bc2063a4dfa4c92b85ede967373d
varmeh <varun.sagi20@gmail.com>
2019-07-18 19:06:15 +0530

    changing server port to 4000 as react-app default port is 3000

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ const app = express()
 configureMiddlewares(app)
 integrateRoutes(app)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 app.listen(port, () => {
 	console.log(`Server on http://localhost:${port}`)
