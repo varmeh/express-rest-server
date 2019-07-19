28ece503689f2bd22cd4e75e6f55ae3d788f6db1
varmeh <varun.sagi20@gmail.com>
2019-07-19 16:12:40 +0530

    post model

---
+++ app/models/post.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose')
+
+const postSchema = new Schema(
+	{
+		title: {
+			type: String,
+			required: true
+		},
+		content: {
+			type: String,
+			required: true
+		},
+		imageUrl: {
+			type: String,
+			required: true
+		},
+		creator: {
+			name: {
+				type: String
+			}
+		}
+	},
+	{ timestamps: true }
+)
+
+module.exports = model('Post', postSchema)
