9315ed17eef91905829975f43a699ebe8f94246e
varmeh <varun.sagi20@gmail.com>
2019-07-19 15:35:28 +0530

    mongoose integration

--- app/app.js
+++ app/app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const configureMiddlewares = require('./middleware')
 const integrateRoutes = require('./route.integrator')
 
@@ -9,6 +11,19 @@ integrateRoutes(app)
 
 const port = process.env.PORT || 4000
 
-app.listen(port, () => {
-	console.log(`Server on http://localhost:${port}`)
-})
+const run = async () => {
+	try {
+		const result = await mongoose.connect(process.env.MONGO_URI, {
+			useNewUrlParser: true,
+			poolSize: 10
+		})
+		console.log(result)
+		app.listen(port, () => {
+			console.log(`Server on http://localhost:${port}`)
+		})
+	} catch (error) {
+		console.error('Mongoose Connection failed with error', error)
+	}
+}
+
+run()
