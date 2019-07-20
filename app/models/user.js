71ad2a77336eb23fc337e476ea672e331b77a323
varmeh <varun.sagi20@gmail.com>
2019-07-20 16:25:22 +0530

    user model

---
+++ app/models/user.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema({
+	email: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	name: {
+		type: String,
+		required: true
+	},
+	status: {
+		type: String,
+		required: true
+	},
+	posts: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'Post'
+		}
+	]
+})
+
+module.exports = model('User', userSchema)
