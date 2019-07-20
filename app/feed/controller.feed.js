36605130388b7d5b409491a418e0684c1de116c1
varmeh <varun.sagi20@gmail.com>
2019-07-20 14:28:10 +0530

    post update implmentation

--- app/feed/controller.feed.js
+++ app/feed/controller.feed.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const path = require('path')
+
 const { validationResult } = require('express-validator')
 
 const { ErrorResponse } = require('../error.manager')
@@ -54,9 +57,9 @@ exports.getPost = async (req, res, next) => {
 	try {
 		const post = await Post.findById(postId)
 		if (!post) {
-			const error = new Error('Post not found!')
-			error.statusCode = 404
-			throw error
+			return next(
+				new ErrorResponse(404, 'Post not found!', ['Post not found!'])
+			)
 		}
 		res.status(200).json({ post })
 	} catch (error) {
@@ -64,4 +67,46 @@ exports.getPost = async (req, res, next) => {
 			new ErrorResponse(error.statusCode || 500, error.message, [error.message])
 		)
 	}
+}
+
+exports.updatePost = async (req, res, next) => {
+	/* Handling errors during validation phase */
+	const errors = validationResult(req)
+	if (!errors.isEmpty()) {
+		// We have validation error
+		const errorArray = errors
+			.array()
+			.map(({ value, msg, param }) => `'${param}' has ${msg}: ${value}`)
+		return next(new ErrorResponse(422, 'Validation failure', errorArray))
+	}
+
+	try {
+		const post = await Post.findById(req.params.postId)
+		if (!post) {
+			return next(
+				new ErrorResponse(404, 'Post not found!', ['Post not found!'])
+			)
+		}
+		const { title, content } = req.body
+		post.title = title
+		post.content = content
+
+		if (req.file) {
+			// Delete old image
+			clearImage(post.imageUrl)
+
+			// User has provided new image
+			post.imageUrl = req.file.path
+		}
+		const result = await post.save()
+		res.status(200).json({ post: result })
+	} catch (error) {
+		next(new ErrorResponse(500, 'Db Save failure', [error.message]))
+	}
+}
+
+const clearImage = filePath => {
+	filePath = path.join(__dirname, '..', '..', 'public', filePath)
+	console.log(`Deleting file: ${filePath}`)
+	fs.unlink(filePath, err => console.error(err))
 }
--- app/feed/route.feed.js
+++ app/feed/route.feed.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const { body } = require('express-validator')
 
-const { getFeedPosts, createPost, getPost } = require('./controller.feed')
+const {
+	getFeedPosts,
+	createPost,
+	getPost,
+	updatePost
+} = require('./controller.feed')
 const router = express.Router()
 
 router.get('/posts', getFeedPosts)
@@ -18,5 +23,7 @@ const userPostValidators = [
 router.post('/post', userPostValidators, createPost)
 
 router.get('/post/:postId', getPost)
+
+router.put('/post/:postId', userPostValidators, updatePost)
 
 module.exports = router
